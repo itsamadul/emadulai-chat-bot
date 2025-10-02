93cd4592a3c5ce0a6fb52e120ddce9cf2bd8464c
Emadul Tarafdar <151501426+itsamadul@users.noreply.github.com>
2025-10-03 02:53:18 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ OpenAI Image Bot is running!"
+    return "✅ Gemini Bot is running!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
